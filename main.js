e65710e91910a3c3b1bd1bdaebebb0a31f1fc4e5
LazyDog Games <73097891+gseok89@users.noreply.github.com>
2021-09-24 22:03:55 +0900

    팝업 구현 완료

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+
+//오버레이 클릭 시 이벤트
 $('.overlay').click(function(){
     $(this).toggleClass("click");
     $('.sidebar').toggleClass("show");
-    randPos();
-    // $('.popup').toggleClass("show");
+    // randPos();
 });
+
 $('.sub-btn').click(function(){
     $('nav ul .sub-show').toggleClass("show");
 });
@@ -19,79 +21,10 @@ function randPos(){
   $('.popup').css('top', randPosY);
 }
 
-//마우스 트레일
-// document.addEventListener('mousemove', circleTrail);
-
-// let colors = ['#00FFFF', '#DC143C', '	#7CFC00', '#6A5ACD', '#FF8C00'];
-
-// function circleTrail(e) {
-//   const circle = document.createElement('div');
-//   circle.className = 'circle';
-//   document.body.appendChild(circle);
-  
-//   circle.style.left = e.x + 'px';
-//   circle.style.top = e.y + 'px';
-  
-// //   let color = colors[Math.floor(Math.random() * colors.length)];
-// //   circle.style.borderColor = color;
-  
-//   circle.style.transition = "all 0.5s linear";
-//   circle.style.left = circle.offsetLeft - 20 + 'px';
-//   circle.style.Top = circle.offsetTop - 20 + 'px';
-  
-//   circle.style.width = '30px';
-//   circle.style.height = '30px';
-//   circle.style.borderWidth = '2px';
-//   circle.style.opacity = 0;
-  
-// }
 
 $(document).ready(function() {
-  // var el1 = $(".grid-1-item");
-  // for (i=0 ; i<1000 ; i++) {
-  //   var newEl1 = el1.clone();
-  //   $(".grid-1").append(newEl1);
-  // }
 
-  // var gridNumber=16
-  // var gridSize=600/gridNumber;
-
-  // var $grid = $('#box')
-  // var $gridItem = $('.grid')
-  // var gridItemHeight = $gridItem.height()
-  // var gridItemWidth = $gridItem.width()
-  
-  // var horzCount = Math.floor($grid.width() / gridItemWidth)
-  // var vertCount = Math.floor($grid.height() / gridItemHeight)
-  
-  // var totalGridItems = horzCount * vertCount
-
-
-
-  // /*populate with divs*/
-
-  // var func= function(){
-  //   for(var i=0; i<totalGridItems;i++) {
-  // $('#box').append("<div class ='grid'></div>");
-
-  // }
-
-  // }
-
-  // func();
-
-
-  // var high= function(){
-  // $('.grid').hover(function() {
-  // /*
-  //   $('.highlight').css({backround-color:"#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6)"});
-  // */
-  //   $(this).addClass('highLight');
-  // });
-  // }
-  // high();
-
-
+  //마우스 픽셀 흔적
   var $grid = $('.grid-1')
   var $gridItem = $('.grid-1-item')
   var gridItemHeight = $gridItem.height()
@@ -113,5 +46,67 @@ $(document).ready(function() {
   });
   }
   high();
+
+
+    $('.popup-close-btn').click(function () {
+      $(this).parents('.popup').hide()
+  });
+
+  // $('.popup-content').mouseenter(function () {
+  //     $('.arrow2').css({
+  //         'top': '-110px'
+  //     });
+  //     $('.arrow1').animate({
+  //         'top': '110px'
+  //     }, 250);
+  //     $('.arrow2').animate({
+  //         'top': '0'
+  //     }, 250);
+  // });
+
+  // $('.popup-content').mouseleave(function () {
+  //     $('.arrow1, .arrow2').css({
+  //         'top': '0'
+  //     });
+  // });
+
+  // $('.popup-open-btn').click(function () {
+  //     $('.popup-bottom').css({'display': 'block'});
+  //     $('.popup--5').addClass('ex');
+  //     $('.popup-m--7').addClass('ex');
+  //     $(this).fadeOut(250);
+  // });
 });
+
+var popupCount = 20;
+
+            $("#draggable1").draggable({
+                start: function () {
+                    popupCount++;
+                    updateCounterStatus($('.popup--1'), popupCount);
+                }
+            });
+
+            $("#draggable2").draggable({
+              start: function () {
+                  popupCount++;
+                  updateCounterStatus($('.popup--2'), popupCount);
+              }
+          });
+
+            function updateCounterStatus($event_counter, new_count) {
+                if (!$event_counter.hasClass("ui-state-hover")) {
+                    $event_counter.addClass("ui-state-hover")
+                        .siblings().removeClass("ui-state-hover");
+                }
+
+                $event_counter.css('z-index', new_count);
+            }
+
+            $('.popup').click(function () {
+                var zCount = popupCount++
+                $(this).css({
+                    'z-index': zCount
+                });
+            });
 
