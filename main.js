6e4a49f01ceca1fec81bd4ba0e5d7b0203a15153
sjpark0645 <tjdwns1006@naver.com>
2021-09-26 21:47:47 +0900

    nav 대폭수정

--- main.js
+++ main.js
@@ -6,9 +6,17 @@ $('.overlay').click(function(){
     // randPos();
 });
 
-$('.sub-btn').click(function(){
-    $('nav ul .sub-show').toggleClass("show");
-});
+// $('.sub-btn').click(function(){
+//    $('nav ul .sub-show').toggleClass("show");
+//});
+
+
+$(".menu-v1").click(function(){
+  $(this).children(".submenu").stop().slideDown(); 
+}); 
+
+
+
 
 //랜덤 팜업 함수
 function randPos(){
